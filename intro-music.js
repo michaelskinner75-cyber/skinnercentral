0f3f7f306ae32451c11cb029c01f0687657d4312
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-31 16:01:56 +0100

    Bust homepage enhancement cache

--- intro-music.js
+++ intro-music.js
@@ -1,4 +1,5 @@
 (()=>{
-  const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)});
+  const stamp='20260731-1559';
+  const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=`${src}?v=${stamp}`;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)});
   load('intro-music-core.js').then(()=>load('netflix-home.js')).catch(error=>console.error('Could not load homepage enhancements.',error));
 })();
