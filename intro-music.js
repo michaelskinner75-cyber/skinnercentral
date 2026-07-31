250adaf7a2e5461f31985add26e92cfa33871548
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-31 16:45:18 +0100

    Load featured Battleships homepage update

--- intro-music.js
+++ intro-music.js
@@ -1,5 +1,8 @@
 (()=>{
-  const stamp='20260731-1559';
+  const stamp='20260731-1644';
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=`${src}?v=${stamp}`;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)});
-  load('intro-music-core.js').then(()=>load('netflix-home.js')).catch(error=>console.error('Could not load homepage enhancements.',error));
+  load('intro-music-core.js')
+    .then(()=>load('netflix-home.js'))
+    .then(()=>load('homepage-featured-battleships.js'))
+    .catch(error=>console.error('Could not load homepage enhancements.',error));
 })();
